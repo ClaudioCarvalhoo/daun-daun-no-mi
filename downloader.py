479e6c5c7b83d87133d19a1763084d1908a897cf
Claudio Carvalho <cco2@cin.ufpe.br>
2024-01-07 02:38:25 +0000

    scraper + downloader

---
+++ downloader.py
@@ -0,0 +1,28 @@
+import os
+import json
+import requests
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"
+}
+
+with open("image_urls.json", "r") as f:
+    data = json.load(f)
+
+chapter_start = 3
+chapter_end = 120
+for chapter in range(chapter_start, chapter_end + 1):
+    print(f"Chapter {chapter}")
+
+    directory = f"./out/{chapter}"
+    if not os.path.exists(directory):
+         os.makedirs(directory) 
+
+    for i, page_url in enumerate(data[str(chapter)]):
+        print(f"Page {i+1}")
+
+        res = requests.get(page_url, headers=headers)
+        with open(f"{directory}/{i+1}.png", 'wb') as f:
+                f.write(res.content)
+
+    print("==============")
---
+++ scraper.py
@@ -0,0 +1,45 @@
+import json
+from collections import OrderedDict
+
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+url = "https://onepieceex.net/mangas/leitor/"
+options = Options()
+options.add_argument('--headless')
+driver = webdriver.Firefox(options=options)
+
+chapter_start = 42
+chapter_end = 1103
+
+for chapter in range(chapter_start, chapter_end + 1):
+    print(f"Chapter {chapter}")
+
+    driver.get(url + str(chapter))
+
+    element_present = EC.presence_of_element_located((By.CLASS_NAME, 'paginamanga'))
+    WebDriverWait(driver, 600).until(element_present)
+
+    pages = []
+    while True:
+        print(f"Page {len(pages) + 1}")
+        page = driver.find_element(By.CLASS_NAME, 'paginamanga')
+        src = page.get_attribute('src')
+        if len(pages) != 0 and src == pages[-1]:
+            break
+        pages.append(src)
+        page.click()
+
+    with open("image_urls.json", "r") as f:
+        data = json.load(f)
+    data[str(chapter)] = pages
+    sorted_data = OrderedDict(sorted(data.items(), key=lambda x: int(x[0])))
+    with open("image_urls.json", "w") as f:
+        f.write(json.dumps(sorted_data, sort_keys=False, indent=4, separators=(',', ': ')))
+
+    print("==============")
+
+driver.close()
